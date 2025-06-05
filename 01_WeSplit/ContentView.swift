d5c75a94daa4970587f5c83f3fde62a2e5f990e4
truongnguyen3 <truong.nguyen3@niteco.se>
2025-06-05 09:48:51 +0700

    Initial Commit

---
+++ 01_WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01_WeSplit
+//
+//  Created by Truong Huu Nguyen on 5/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01_WeSplit/_1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_WeSplitApp.swift
+//  01_WeSplit
+//
+//  Created by Truong Huu Nguyen on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct _1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
