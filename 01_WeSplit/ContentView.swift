9f9de8f6084c0518e69e2a3aaa045ecf02dedb29
truongnguyen3 <truong.nguyen3@niteco.se>
2025-06-05 14:42:15 +0700

    add custom toolbar

--- 01_WeSplit/ContentView.swift
+++ 01_WeSplit/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     private let tipPercentages = [10, 15, 20, 25, 0]
     
@@ -31,6 +32,7 @@ struct ContentView: View {
                         Text("Amount")
                     })
                     .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -56,6 +58,14 @@ struct ContentView: View {
                 
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
         
     }
